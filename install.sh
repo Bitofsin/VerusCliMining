9aa489b1b790fca5ad63fea1beffb081dbd24415
Bitofsin <90807474+Bitofsin@users.noreply.github.com>
2023-08-07 16:30:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
-
 mkdir ~/ccminer
 cd ~/ccminer
 wget https://github.com/Oink70/Android-Mining/releases/download/v3.8.3-4/ccminer-3.8.3-4_ARM -O ~/ccminer/ccminer
 wget https://raw.githubusercontent.com/Bitofsin/VerusCliMining/blob/main/config.json -O ~/ccminer/config.json
 chmod +x ~/ccminer/ccminer
-
-cat << EOF > ~/ccminer/start.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config.json
-EOF
-chmod +x start.sh
